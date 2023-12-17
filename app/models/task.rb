9da4ed8b0546a55418133509a13fb7c0c498afc6
nanoha <nanoha.creator@gmail.com>
2023-12-17 13:39:04 +0000

    statusをenumへ

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  enum status: { not_started: 0, in_progress: 1, closed: 2 }
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :description, allow_blank: true, length: { minimum: 3, maximum: 500 }
+  validates :start_time, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,4 +20,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_142149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
