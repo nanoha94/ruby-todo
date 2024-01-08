06ed57838e403cdca87bc6a2f31456dc180d676a
nanoha <nanoha.creator@gmail.com>
2024-01-08 10:01:17 +0000

    Enumや日時のバリデーション

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ApplicationRecord
-  enum status: { not_started: 0, in_progress: 1, closed: 2 }
+  enum :status, { not_started: 0, in_progress: 1, closed: 2 }, validate: true
 
   validates :title, presence: true, length: { minimum: 3, maximum: 20 }
   validates :description, allow_blank: true, length: { minimum: 3, maximum: 500 }
-  validates :start_time, presence: true
+  validates :start_time, presence: true, comparison: { greater_than_or_equal_to: Time.current.beginning_of_day }
+  validates :end_time, comparison: { greater_than: :start_time }
 end
